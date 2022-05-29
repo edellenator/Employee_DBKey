1deb6a27fdf44dbee2558dd7c1af457940677241
Erik Dell <edellenator@gmail.com>
2022-05-28 20:53:23 -0700

    main menu

--- index.js
+++ index.js
@@ -11,27 +11,59 @@ const db = require ('./db/connection');
 const cTable = require('console.table');
 const inquirer = require ('inquirer');
 
+async function returnToMain() {
+    let prompt = inquirer.prompt([
+        {
+            type: 'list',
+            name: 'returnToMain',
+            choices: ['Return to Main Menu']
+        }
+    ])
 
+    let response = await prompt;
 
+    if (response.returnToMain === 'Return to Main Menu') {
+        init();
+    }
+    
+}
+async function init() {
+    let prompt = inquirer.prompt([
+        {
+            type: 'list',
+            name: "menuSelect",
+            message: `
+            ==============================
+         |- CHOOSE AN OPTION FROM THE MENU -|
+            ==============================
+            `,
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add a New Department',
+                'Add a New Role',
+                'Add a New Employee',
+                'Update a Department Name',
+                'Update the Salary of a Role',
+                'Update the Role of an Employee'
+            ]
+            
+        }
+    ])
 
+    let response = await prompt
+    console.log(response.menuSelect)
 
+    if (response.menuSelect === 'View All Departments') {
+        department.viewAllDepartments();
+        returnToMain();
+    }
+    else if (response.menuSelect === 'View All Roles') {
+        role.viewAllRoles();
+        returnToMain();
+    }
+    
+}
 
-var test = role.viewAllRoles();
-
-console.log(test);
-
-// async function mainMenu() {
-//     let response = await inquirer.prompt([
-//         {
-//             type: 'input',
-//             name: "email",
-//             message: "input an email"
-//         }
-//     ])
-
-//     console.log(response)
-// }
-
-// mainMenu().then(response => {
-//     if (response === )
-// })
+init();
